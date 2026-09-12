ed6235e19ed5e00308d6255a667a043a9a2cb864
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-09-12 18:51:15 +0400

    feat(nvim): disable completion in markdown

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -2,6 +2,10 @@ return {
   {
     "saghen/blink.cmp",
     opts = function(_, opts)
+      opts.enabled = function()
+        return vim.bo.filetype ~= "markdown"
+      end
+
       opts.sources.default = vim.tbl_filter(function(source)
         return source ~= "buffer"
       end, opts.sources.default)
