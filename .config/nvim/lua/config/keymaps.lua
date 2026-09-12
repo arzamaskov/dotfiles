a18e506e2a18b543727e59ed46d44a3073724ea7
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-09-12 10:05:15 +0400

    feat: add custom keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local bind = vim.keymap.set
+local opts = { silent = true }
+
+-- Русская раскладка
+bind("i", "<C-х>", "<C-[>", opts)
+
+bind("n", "р", "h", opts)
+bind("n", "о", "gj", opts)
+bind("n", "л", "gk", opts)
+bind("n", "д", "l", opts)
+
+bind("i", "<C-г>", "<C-G>u<C-U>", opts)
+bind("i", "<C-ц>", "<C-G>u<C-W>", opts)
+
+-- Показать текущий файл в Explorer
+bind("n", "<C-n>", function()
+  Snacks.explorer.reveal()
+end, { desc = "Reveal file in explorer" })
+
+-- Последние буферы
+bind("n", "<leader>;", function()
+  Snacks.picker.buffers({
+    sort_lastused = true,
+    current = false,
+  })
+end, { desc = "Recent buffers" })
