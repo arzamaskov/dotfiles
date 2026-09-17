802b9fb1b6cb1bed249f89743591e2ef7f769a87
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-09-17 16:09:33 +0400

    chore(zsh): include time in journal sync commits

--- .config/zsh/functions/journal.zsh
+++ .config/zsh/functions/journal.zsh
@@ -25,8 +25,12 @@ journal() {
       journal_history "$file" "$command"
       ;;
 
+    sync)
+      journal_sync
+      ;;
+
     *)
-      echo "usage: journal [today|MM-DD]" >&2
+      echo "usage: journal [today|MM-DD|sync]" >&2
       return 1
       ;;
   esac
@@ -60,8 +64,17 @@ journal_history() {
   ' "$file"
 }
 
-jsync() {
-  git_backup "$JOURNAL_DIR" "log from $(date '+%Y-%m-%d')"
+journal_sync() {
+  git -C "$JOURNAL_DIR" pull --ff-only || return 1
+
+  git -C "$JOURNAL_DIR" add index.txt
+
+  if ! git -C "$JOURNAL_DIR" diff --cached --quiet; then
+    git -C "$JOURNAL_DIR" commit \
+      -m "journal update: $(date '+%Y-%m-%d %H:%M')" || return 1
+  fi
+
+  git -C "$JOURNAL_DIR" push origin
 }
 
 jr() {
