ef70118a59b079ef62e7d727de92188eace3affd
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-09-18 16:15:23 +0400

    feat(nvim): add journal day keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -33,3 +33,59 @@ map("x", "<leader>cc", "gc", { remap = true, desc = "Toggle comment" })
 
 -- TreeSJ
 map("n", "<leader>j", "<cmd>TSJToggle<cr>", { desc = "Toggle split/join" })
+
+-- Journal
+local journal = vim.fn.expand("~/journal/index.txt")
+
+local function setup_journal_keymap(buf)
+  local path = vim.api.nvim_buf_get_name(buf)
+
+  if path ~= journal then
+    return
+  end
+
+  vim.keymap.set("n", "<leader>J", function()
+    local now = os.date("*t")
+    local weekdays = {
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    }
+
+    local header = string.format("%04d-%02d-%02d %s", now.year, now.month, now.day, weekdays[now.wday])
+
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+    for _, line in ipairs(lines) do
+      if line == header then
+        vim.notify("Today's journal entry already exists")
+        return
+      end
+    end
+
+    vim.api.nvim_buf_set_lines(buf, 0, 0, false, {
+      header,
+      "",
+      "",
+      "",
+    })
+
+    vim.api.nvim_win_set_cursor(0, { 3, 0 })
+    vim.cmd("startinsert")
+  end, {
+    buffer = buf,
+    desc = "Add today's journal entry",
+  })
+end
+
+setup_journal_keymap(0)
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function(args)
+    setup_journal_keymap(args.buf)
+  end,
+})
